ba222b9475cb9075d578e910386164d997a93696
macamarambio <marambio.macarena@gmail.com>
2017-11-07 23:11:00 -0300

    modificando evento Dom

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,47 @@
-var boton = document.getElementById('btn');
-
 //Agregando Evento click para cambiar submit de boton a input
-function addList() {
-	var text = getElementById('submit-1');
+ document.addEventListener ('click', function addList() {
+	var text = getElementById ('submit1');
+	var text2 = getElementById ('submit2');
 
-	if(text.classList.contains('show-list')){
+	if(text.classList.contains ('show-list') && text2.classList.contains('ocult-list')){
+		// cambiando texto 1 de visible a invisible
+		text.classList.remove ('show-list');
+		text.classList.add ('ocult-list');
+		// cambiando texto 2 de invisible a visible
+		text2.classList.remove ('ocult-list');
+		text2.classList.add ('show-list');
+	}else{
+		text.classList.remove ('ocult-list');
+		text.classList.add ('show-list');
+
+		text2.classList.remove ('show-list');
+		text2.classList.add ('ocult-list');
+	}
+});
+
+
+
+
+
+ 	/*//Agregando Evento click para cambiar submit de boton a input
+function addList() {
+	var text = getElementById('submit1');
+	var text2 = getElementById('submit2');
+
+	if(text.classList.contains('show-list') && text2.classList.contains('ocult-list')){
+		// cambiando texto 1 de visible a invisible
 		text.classList.remove('show-list');
-		menu.classList.add('ocult-list');
+		text.classList.add('ocult-list');
+		// cambiando texto 2 de invisible a visible
+		text2.classList.remove('ocult-list');
+		text2.classList.add('show-list');
 	}else{
 		text.classList.remove('ocult-list');
 		text.classList.add('show-list');
+
+		text2.classList.remove('show-list');
+		text2.classList.add('ocult-list');
 	}
 }
 
- boton.addEventListener('click', addList);
+ document.addEventListener('click', addList);*/
