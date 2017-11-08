dbd1a806132bf103165e892f6a10292465b5f3d9
macamarambio <marambio.macarena@gmail.com>
2017-11-08 10:07:32 -0300

    segundo intento evento click

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,22 @@
 //Agregando Evento click para cambiar submit de boton a input
-var boton = document.getElementsByClassName('clk')
+var boton = document.getElementsByClassName('principalSection');
+for (var i = 0; i < boton.length; i++) {
+	boton[i].addEventListener('click', addList);
+}
 
 function addList() {
-	var text = document.getElementById('submit1');
-	var text2 = document.getElementById('submit2');
+	var text = document.getElementById('clk');
 
 	if(text.classList.contains('show-list') && text2.classList.contains('ocult-list')){
 		// cambiando texto 1 de visible a invisible
 		text.classList.remove('show-list');
 		text.classList.add('ocult-list');
-		// cambiando texto 2 de invisible a visible
-		text2.classList.remove('ocult-list');
-		text2.classList.add('show-list');
 	}else{
 		text.classList.remove('ocult-list');
 		text.classList.add('show-list');
-
-		text2.classList.remove('show-list');
-		text2.classList.add('ocult-list');
 	}
 };
 
-boton.addEventListener('click', addList)
  
 
 
